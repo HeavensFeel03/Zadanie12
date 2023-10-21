2499bab37acad0aed2be7e807ed9aaefa32c22cd
Bato <bato19941994@bk.ru>
2023-10-21 17:36:05 +0800

    updateTodosTest

--- src/main/java/ru/netology/tasks/Todos.java
+++ src/main/java/ru/netology/tasks/Todos.java
@@ -33,8 +33,10 @@ public class Todos {
             if (task.matches(query)) { // если задача подходит под запрос
                 result = addToArray(result, task); // добавляем её в массив ответа
             }
+
         }
         return result;
+
     }
 
     /**
--- src/test/java/ru/netology/tasks/TodosTest.java
+++ src/test/java/ru/netology/tasks/TodosTest.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 
 public class TodosTest {
 
-    SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+    SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+    SimpleTask simpleTask2 = new SimpleTask(7, "Позвонить бабушке");
     String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
     Epic epic = new Epic(55, subtasks);
     Meeting meeting = new Meeting(
@@ -14,16 +15,22 @@ public class TodosTest {
             "Приложение НетоБанка",
             "Во вторник после обеда"
     );
+    Meeting meeting1 = new Meeting(
+            555,
+            "Позвонить дедушке",
+            "Звонок",
+            "Во среду после обеда"
+    );
 
 
     @Test
     public void shouldAddThreeTasksOfDifferentType() {
         Todos todos = new Todos();
-        todos.add(simpleTask);
+        todos.add(simpleTask1);
         todos.add(epic);
         todos.add(meeting);
 
-        Task[] expected = {simpleTask, epic, meeting};
+        Task[] expected = {simpleTask1, epic, meeting};
         Task[] actual = todos.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -31,11 +38,11 @@ public class TodosTest {
     @Test
     public void shouldSearchSimpleTask() {
         Todos todos = new Todos();
-        todos.add(simpleTask);
+        todos.add(simpleTask1);
         todos.add(epic);
         todos.add(meeting);
 
-        Task[] expected = {simpleTask};
+        Task[] expected = {simpleTask1};
         Task[] actual = todos.search("Позвонить родителям");
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -43,7 +50,7 @@ public class TodosTest {
     @Test
     public void shouldSearchEpic() {
         Todos todos = new Todos();
-        todos.add(simpleTask);
+        todos.add(simpleTask1);
         todos.add(epic);
         todos.add(meeting);
 
@@ -53,9 +60,50 @@ public class TodosTest {
     }
 
     @Test
+    public void shouldSearchMore2() {
+        Todos todos = new Todos();
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask1, simpleTask2};
+        Task[] actual = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMore3() {
+        Todos todos = new Todos();
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(epic);
+        todos.add(meeting);
+        todos.add(meeting1);
+
+        Task[] expected = {simpleTask1, simpleTask2, meeting1};
+        Task[] actual = todos.search("Позвонить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void NoShouldSearch() {
+        Todos todos = new Todos();
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Поздравить");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+    @Test
     public void shouldSearchMeeting() {
         Todos todos = new Todos();
-        todos.add(simpleTask);
+        todos.add(simpleTask1);
         todos.add(epic);
         todos.add(meeting);
 
